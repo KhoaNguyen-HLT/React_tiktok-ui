c38c0eecbf2f9bfdbe024a4cb390389737e3df74
VAN KHOA_Nguyen <vankhoa.nguyen@vn.panasonic.com>
2023-11-17 17:31:09 +0700

    Khoa111

--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,5 +1,8 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return <header className={cx('wrapper')}></header>;
     // return <h2>Sidebar</h2>;
 }
 
